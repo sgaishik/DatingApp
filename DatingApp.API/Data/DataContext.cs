bee81de9facfabbed64510d2ca88a90910ccb811
Aishik Sengupta <sg.aishik006@gmail.com>
2020-08-22 10:07:34 +0530

    Initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Value> Values { get; set; } //name of  the table when we scaffold the database
+    }
+}
